b1ea0292701aa42d2f7187175b5c04f6c0721d60
Lucas Sanches <luucaassanches@gmail.com>
2025-07-30 15:10:59 -0300

    Adicionar arquivos de projeto.

---
+++ Mauiapp_Login/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Mauiapp_Login
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Mauiapp_Login/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Mauiapp_Login
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
